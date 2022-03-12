cc8f075c26653a46c757cf705d6cd7fe5cedf823
Trevor Sawler <trevor.sawler@gmail.com>
2022-03-12 16:30:24 -0400

    render function

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -9,20 +9,24 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("./cmd/web/templates/home.page.tmpl")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		if err := tmpl.Execute(w, nil); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-
+		render("home.page.tmpl", w)
 	})
+
 	fmt.Println("Starting front end service on port 80")
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Panicln(err)
+	}
+}
+
+func render(t string, w http.ResponseWriter) {
+	tmpl, err := template.ParseFiles(fmt.Sprintf("./cmd/web/templates/%s", t))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
