298c0ac1bf169a802f7d62f394bd740b8983a978
Trevor Sawler <trevor.sawler@gmail.com>
2022-03-13 16:33:18 -0300

    move connection to rabbit to its own function

--- queue-listener-service/consumer/consumer.go
+++ queue-listener-service/consumer/consumer.go
@@ -2,44 +2,25 @@ package main
 
 import (
 	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/tsawler/go-rabbit/lib/event"
 	"log"
 	"math"
 	"os"
 	"time"
-
-	amqp "github.com/rabbitmq/amqp091-go"
-	"github.com/tsawler/go-rabbit/lib/event"
 )
 
 func main() {
+	// rabbitConn is our connection to RabbitMQ
 	var rabbitConn *amqp.Connection
-	var counts int64
-	var backOff = 1 * time.Second
 
-	// don't continue until rabbitmq is ready
-	for {
-		connection, err := amqp.Dial("amqp://guest:guest@rabbitmq")
-		if err != nil {
-			fmt.Println("RabbitMQ not ready...")
-			counts++
-		} else {
-			fmt.Println()
-			rabbitConn = connection
-			break
-		}
-
-		if counts > 5 {
-			// if we can't connect after five tries, something is wrong...
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		fmt.Printf("Backing off for %d seconds...\n", int(math.Pow(float64(counts), 2)))
-		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
-		time.Sleep(backOff)
-		continue
-
+	// try to connect to RabbitMQ
+	c, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
 	}
-
+	rabbitConn = c
 	defer rabbitConn.Close()
 
 	// start listening for messages
@@ -52,4 +33,36 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+// connect tries to connect to RabbitMQ, and delays between attempts.
+// If we can't connect after 5 tries (with increasing delays), return an error
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// don't continue until rabbitmq is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			fmt.Println("RabbitMQ not ready...")
+			counts++
+		} else {
+			connection = c
+			fmt.Println()
+			break
+		}
+
+		if counts > 5 {
+			// if we can't connect after five tries, something is wrong...
+			fmt.Println(err)
+			return nil, err
+		}
+		fmt.Printf("Backing off for %d seconds...\n", int(math.Pow(float64(counts), 2)))
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		time.Sleep(backOff)
+		continue
+	}
+	return connection, nil
 }
