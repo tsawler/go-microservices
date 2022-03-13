516b56508766f4075f601fe10366a8253c296fb6
Trevor Sawler <trevor.sawler@gmail.com>
2022-03-13 16:25:10 -0300

    comments

--- logger-service/rpc.go
+++ logger-service/rpc.go
@@ -10,17 +10,20 @@ import (
 // over RPC, as long as they are exported.
 type RPCServer struct{}
 
+// LogEntry is the type for writing data to mongo
 type LogEntry struct {
 	Name      string
 	Data      string
 	CreatedAt time.Time
 }
 
+// RPCPayload is the type for data we receive from RPC
 type RPCPayload struct {
 	Name string
 	Data string
 }
 
+// LogInfo writes our payload to mongo
 func (r *RPCServer) LogInfo(payload RPCPayload, resp *string) error {
 	infoLog.Println("Processed payload:", payload)
 
