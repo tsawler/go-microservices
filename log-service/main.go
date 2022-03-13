668e1f1d1b2b536304ddb98379eb1fc3a99ba5ae
Trevor Sawler <trevor.sawler@gmail.com>
2022-03-13 11:07:59 -0300

    add mongo to logs

--- log-service/main.go
+++ log-service/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net"
 	"net/rpc"
 	"os"
+	"time"
 )
 
 var infoLog *log.Logger
+var client *mongo.Client
 
 func main() {
 	infoLogFile, err := os.OpenFile("./logs/logger/info_log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
@@ -17,6 +22,17 @@ func main() {
 	}
 	defer infoLogFile.Close()
 	infoLog = log.New(infoLogFile, "INFO\t", log.Ldate|log.Ltime)
+
+	mongoClient, err := connect()
+	client = mongoClient
+
+	ctx, _ := context.WithTimeout(context.Background(), 15*time.Second)
+	// close connection when func exits
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
 
 	log.Println("Starting RPC Server on port 5001")
 	err = rpc.Register(new(RPCServer))
@@ -37,4 +53,22 @@ func main() {
 		}
 		go rpc.ServeConn(rpcConn)
 	}
+}
+
+func connect() (*mongo.Client, error) {
+	// create connect options
+	clientOptions := options.Client().ApplyURI("mongodb://mongo:27017")
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// Connect to the MongoDB and return Client instance
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		fmt.Println("mongo.Connect() ERROR:", err)
+		return nil, err
+	}
+
+	return client, nil
 }
--- log-service/rpc.go
+++ log-service/rpc.go
@@ -1,11 +1,33 @@
 package main
 
+import (
+	"context"
+	"log"
+	"time"
+)
+
 // RPCServer is the type for our RPC Server. Methods that take this as a receiver are available
 // over RPC, as long as they are exported.
 type RPCServer struct{}
 
+type LogEntry struct {
+	Data      string
+	CreatedAt time.Time
+}
+
 func (r *RPCServer) LogInfo(payload string, resp *string) error {
 	infoLog.Println("Processed payload:", payload)
+
+	collection := client.Database("logs").Collection("logs")
+	_, err := collection.InsertOne(context.TODO(), LogEntry{
+		Data:      payload,
+		CreatedAt: time.Now(),
+	})
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
 	*resp = "Processed payload: " + payload
 	return nil
 }
