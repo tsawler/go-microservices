6cd99d4830311aa2297c76133cd242d61176b366
Trevor Sawler <trevor.sawler@gmail.com>
2022-03-13 10:26:58 -0300

    in progress

--- log-service/main.go
+++ log-service/main.go
@@ -5,12 +5,21 @@ import (
 	"log"
 	"net"
 	"net/rpc"
+	"os"
 )
 
+var infoLog *log.Logger
+
 func main() {
-	//go listenRPC()
+	infoLogFile, err := os.OpenFile("./logs/logger/info_log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening error log file: %v", err)
+	}
+	defer infoLogFile.Close()
+	infoLog = log.New(infoLogFile, "INFO\t", log.Ldate|log.Ltime)
+
 	log.Println("Starting RPC Server on port 5001")
-	err := rpc.Register(new(RPCServer))
+	err = rpc.Register(new(RPCServer))
 	if err != nil {
 		return
 	}
--- log-service/rpc.go
+++ log-service/rpc.go
@@ -1,14 +1,11 @@
 package main
 
-import (
-	"log"
-)
-
 // RPCServer is the type for our RPC Server. Methods that take this as a receiver are available
 // over RPC, as long as they are exported.
 type RPCServer struct{}
 
 func (r *RPCServer) LogInfo(payload string, resp *string) error {
-	log.Println("Processed rpc call")
+	infoLog.Println("Processed payload:", payload)
+	*resp = "Processed payload: " + payload
 	return nil
 }
