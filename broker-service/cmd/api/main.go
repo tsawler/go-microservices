c054bb3c0e0e2e3f845b1de66a14c38a69ce82e2
Trevor Sawler <trevor.sawler@gmail.com>
2022-03-21 19:05:44 -0300

    comments

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -55,11 +55,14 @@ func main() {
 	go app.watchEtcd()
 
 	log.Println("Starting broker service on port", webPort)
+
+	// define the http server
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
 	}
 
+	// start the server
 	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
