18161b0d8c7b1970ab6a5f8f34cc8a8af2fc5cc3
Trevor Sawler <trevor.sawler@gmail.com>
2022-04-06 18:06:54 -0300

    use embedded fs for templates

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"html/template"
 	"log"
@@ -21,25 +22,29 @@ func main() {
 	}
 }
 
+//go:embed templates
+var templateFS embed.FS
+
 // render generates a page of html from our template files
 func render(w http.ResponseWriter, t string) {
 	// all the required templates for any page
 	partials := []string{
-		"./templates/base.layout.gohtml",
-		"./templates/header.partial.gohtml",
-		"./templates/footer.partial.gohtml",
+		"templates/base.layout.gohtml",
+		"templates/header.partial.gohtml",
+		"templates/footer.partial.gohtml",
 	}
 
 	// append the template we received as a parameter
 	var templateSlice []string
-	templateSlice = append(templateSlice, fmt.Sprintf("./templates/%s", t))
+	templateSlice = append(templateSlice, fmt.Sprintf("templates/%s", t))
 
 	for _, x := range partials {
 		templateSlice = append(templateSlice, x)
 	}
 
 	// parse the templates
-	tmpl, err := template.ParseFiles(templateSlice...)
+	//tmpl, err := template.ParseFiles(templateSlice...)
+	tmpl, err := template.ParseFS(templateFS, templateSlice...)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
