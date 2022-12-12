12fbb1549d172aa44ab2b4e72168b905fab407ea
Mahdi <mahdizarepoor123456789@gmail.com>
2022-12-12 15:22:47 +0330

    render without need to create another slice

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -27,30 +27,20 @@ var templateFS embed.FS
 
 // render generates a page of html from our template files
 func render(w http.ResponseWriter, t string) {
-	// all the required templates for any page
 	partials := []string{
-		"templates/base.layout.gohtml",
-		"templates/header.partial.gohtml",
-		"templates/footer.partial.gohtml",
+		"",
+		"./templates/base.layout.gohtml",
+		"./templates/header.partial.gohtml",
+		"./templates/footer.partial.gohtml",
 	}
 
-	// append the template we received as a parameter
-	var templateSlice []string
-	templateSlice = append(templateSlice, fmt.Sprintf("templates/%s", t))
-
-	for _, x := range partials {
-		templateSlice = append(templateSlice, x)
-	}
-
-	// parse the templates
-	//tmpl, err := template.ParseFiles(templateSlice...)
-	tmpl, err := template.ParseFS(templateFS, templateSlice...)
+	partials[0] = fmt.Sprintf("./templates/%s", t)
+	tmpl, err := template.ParseFiles(partials...)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// execute the template
 	if err := tmpl.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
